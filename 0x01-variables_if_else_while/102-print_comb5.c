fe35fa257d13fe998a7816923bb80aa9c402119c
Kikelomo Bamisebi <bamishebikikelomo@gmail.com>
2023-02-19 06:36:05 -0800

    Task 12 completed

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ *Return: Always 0 (Success)
+ */
+
+int main() {
+    int i, j;
+    for (i = 0; i < 100; i++) {
+        for (j = i; j < 100; j++) {
+            if (i != j) {
+                putchar(i/10 + '0');
+                putchar(i%10 + '0');
+                putchar(' ');
+                putchar(j/10 + '0');
+                putchar(j%10 + '0');
+                if (i != 99 || j != 98) {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    return 0;
+}
