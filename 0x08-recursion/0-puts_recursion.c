4c7ceca38c5162c445df9c36f8e8e250a9d64834
Kikelomo Bamisebi <bamishebikikelomo@gmail.com>
2023-03-08 16:26:21 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - print a string followed by a new line);
+ * _puts_recursion - prints string followed by a new line;
  * @s: input
  * Return: Always 0 (Success)
  */
@@ -14,5 +14,5 @@ void _puts_recursion(char *s)
 
 	else
 		_putchar('\n');
-
 }
+
